b0933ef7e22c000fbe81f5d3e04d8b4a845420ee
tongzhou <tongzhou@tongzhoudeMacBook-Pro.local>
2025-11-24 18:23:03 +0800

    add posthog

---
+++ instrumentation-client.ts
@@ -0,0 +1,10 @@
+// instrumentation-client.ts
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "/ingest",
+  ui_host: "https://us.posthog.com",
+  defaults: "2025-05-24",
+  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+  debug: process.env.NODE_ENV === "development",
+});
